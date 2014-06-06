267e0c26dbbcdcd1f7316cc683e1f4d4db8ebbbc
Nejuf <nejuf1@gmail.com>
2014-06-05 17:15:44 -0700

    Get DRY with RESTful controller for models

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,2 @@
-class GamesController < ApplicationController
-
-  def index
-    @games = Game.all
-
-    render json: @games
-  end
-
-  def create
-    @game = Game.new(params[:game])
-
-    if @game.save
-      render json: @game
-    else
-      render json: @game.errors
-    end
-  end
+class GamesController < ObjectController
 end
