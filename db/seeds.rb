e3af41277a0b60b9e9b9acf05d82746fd5efda3c
Nejuf <nejuf1@gmail.com>
2014-06-06 19:14:00 -0700

    Adds seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Beginning seed..."
+
+mod = User.create!({email: 'user1@example.com', password: '12341234'})
+u2 = User.create!({email: 'user2@example.com', password: '12341234'})
+u3 = User.create!({email: 'user3@example.com', password: '12341234'})
+u4 = User.create!({email: 'user4@example.com', password: '12341234'})
+
+g1 = Game.create!({name: 'Game1', seconds_remaining: 10, moderator: mod})
+g2 = Game.create!({name: 'Game2', seconds_remaining: 100, moderator: mod})
+g3 = Game.create!({name: 'Game3', seconds_remaining: 1000, moderator: mod})
+
+t1 = Team.create!({name: 'Team1', users: [u2]})
+t2 = Team.create!({name: 'Team1', users: [u3, u4]})
+
+puts "Seeding done."
